9af85338be11a17fc83732e2dae2b622afdef973
tetianamakar <divnatany17@gmail.com>
2024-06-19 20:26:37 +0300

    cleanup code

--- src/main/java/com/tetianamakar/customers/converter/EntityConverter.java
+++ src/main/java/com/tetianamakar/customers/converter/EntityConverter.java
@@ -1,32 +1,12 @@
 package com.tetianamakar.customers.converter;
 
-import com.tetianamakar.customers.entity.Company;
 import com.tetianamakar.customers.entity.Customer;
-import com.tetianamakar.customers.payload.response.CompanyResponse;
-import com.tetianamakar.customers.payload.response.CustomerResponse;
+import com.tetianamakar.customers.payload.CustomerResponse;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class EntityConverter {
-
-
-
-    public static List<CompanyResponse> convertCompanies(List<Company> companies) {
-        return companies.stream()
-                .map(EntityConverter::convertCompany)
-                .collect(Collectors.toList());
-    }
-
-    private static CompanyResponse convertCompany(Company company) {
-        CompanyResponse response = new CompanyResponse();
-        response.setId(company.getId());
-        response.setName(company.getName());
-        response.setRegistrationNumber(company.getRegistrationNumber());
-        response.setCreatedAt(company.getCreatedAt());
-        response.setAddress(company.getAddress());
-        return response;
-    }
 
     public static CustomerResponse convertCustomer(Customer savedCustomer) {
         CustomerResponse customerResponse = new CustomerResponse();
@@ -43,6 +23,5 @@ public class EntityConverter {
                 .map(EntityConverter::convertCustomer)
                 .collect(Collectors.toList());
     }
-
 
 }
